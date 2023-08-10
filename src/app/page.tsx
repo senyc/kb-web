e76d28a36ddf76e7128a6343af39f775d60626f5
senyc <kdbomhof@gmail.com>
2023-08-10 11:55:13 -0400

    feat: update text size and h2 text on index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Index() {
   return (
     <main className='flex h-full w-full flex-row items-center justify-center overflow-hidden text-center'>
       <div>
-        <h1 className='text-4xl font-bold'>
+        <h1 className='text-5xl font-bold'>
           Kyler Bomhof
         </h1>
-        <h2 className='mt-2 text-lg'>
-          Full-stack developer
+        <h2 className='mt-5 text-lg'>
+          Full-stack software developer
         </h2>
         <div className='mt-2 flex flex-row justify-center gap-6'>
           <ExternalLinks />
