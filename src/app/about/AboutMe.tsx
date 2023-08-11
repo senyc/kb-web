03c3a581d590d1b0fd83003e98386bfbb7260006
senyc <kdbomhof@gmail.com>
2023-08-10 20:38:12 -0400

    feat(about): enlarge body text size

--- src/app/about/AboutMe.tsx
+++ src/app/about/AboutMe.tsx
@@ -1,6 +1,6 @@
 export default function AboutMe() {
   return (
-    <>
+    <div className='text-lg'>
       <p className='mb-8'>
         Kyler is a passionate full-stack developer with professional experience building web applications. He is very interested in, and has done a lot of reasearch regarding software architecture and building applications such that they are testable, and actually tested.
       </p>
@@ -8,6 +8,6 @@ export default function AboutMe() {
       If you are interested in getting the long and short of his work history and experience you can check out his <a className='underline' href='BomhofKylerResume.pdf'>resume</a>. If you would like to get in contact with him directly you can <a className='underline' href='mailto:kdbomhof@gmail.com'>email</a> him, or find him on <a className='underline' href='https://www.linkedin.com/in/kyler-bomhof/'>LinkedIn</a>. Kyler is a firm beleiver of not judging someone by  their cover, but the work the produce, so feel free to visit his <a className='underline' href='https://github.com/senyc'>GitHub</a>.
       </p>
       <div className='mt-1 h-0 border opacity-40'></div>
-    </>
+    </div>
   );
 }
