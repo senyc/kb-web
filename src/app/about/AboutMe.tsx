8cec582b30db7bd624e267c1a18a616496136d0b
senyc <kdbomhof@gmail.com>
2023-08-10 20:56:44 -0400

    feat(about): rewording about me text

--- src/app/about/AboutMe.tsx
+++ src/app/about/AboutMe.tsx
@@ -1,13 +1,12 @@
 export default function AboutMe() {
   return (
-    <div className='text-lg'>
+    <div className='border-b-text border-b-2 text-lg'>
       <p className='mb-8'>
-        Kyler is a passionate full-stack developer with professional experience building web applications. He is very interested in, and has done a lot of reasearch regarding software architecture and building applications such that they are testable, and actually tested.
+        Kyler is a full-stack developer with professional experience building web applications. He is very passionate about software architecture and constructing applications such that they are testable, and actually tested.
       </p>
-      <p>
-      If you are interested in getting the long and short of his work history and experience you can check out his <a className='underline' href='BomhofKylerResume.pdf'>resume</a>. If you would like to get in contact with him directly you can <a className='underline' href='mailto:kdbomhof@gmail.com'>email</a> him, or find him on <a className='underline' href='https://www.linkedin.com/in/kyler-bomhof/'>LinkedIn</a>. Kyler is a firm beleiver of not judging someone by  their cover, but the work the produce, so feel free to visit his <a className='underline' href='https://github.com/senyc'>GitHub</a>.
+      <p className='pb-4'>
+        If you are interested in learning more about his work history and experience, please check out his <a className='underline' target='_blank' href='BomhofKylerResume.pdf'>resume</a>. To get in contact with him directly, you can <a className='underline' target='_blank' href='mailto:kdbomhof@gmail.com'>email</a> him, or find him on <a className='underline' target='_blank' href='https://www.linkedin.com/in/kyler-bomhof/'>LinkedIn</a>. If you would like to see his work firsthand, feel free to visit his <a className='underline' target='_blank' href='https://github.com/senyc'>GitHub</a>.
       </p>
-      <div className='mt-1 h-0 border opacity-40'></div>
     </div>
   );
 }
