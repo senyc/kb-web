e2ca20fde1f7e994afe1e5429c091c73dc770660
senyc <95313103+senyc@users.noreply.github.com>
2023-08-17 18:34:07 -0400

    fix: remove scroll bar transparency, add vertical scroll bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       height: {
         '128': '32rem',
       },
