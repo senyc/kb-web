786fd5252f21cce3153a4850734de0e95ebc5a70
senyc <95313103+senyc@users.noreply.github.com>
2024-03-06 20:37:49 -0500

    feat: updating some static text

--- src/app/about/AboutMe.tsx
+++ src/app/about/AboutMe.tsx
@@ -1,12 +1,10 @@
-import EmailLink from "./EmailLink";
-
 export default function AboutMe() {
   return (
     <div className='border-b-text border-b-2 pb-4 text-lg'>
       <p className='mb-4'>
-        Kyler is a full-stack developer with professional experience building web applications. He is very passionate about software architecture and constructing applications such that they are testable, and actually tested.
+        Kyler is a full-stack developer with professional experience building applications. He is very passionate about software architecture and constructing applications such that they are testable, and actually tested. Right now he is working extensively with Go, Kubernetes, and React.
       </p>
-      If you are interested in learning more about his work history and experience, please check out his <a className='underline' target='_blank' href='BomhofKylerResume.pdf'>resume</a>. To get in contact with him directly, you can  <EmailLink /> him, or find him on <a className='underline' target='_blank' href='https://www.linkedin.com/in/kyler-bomhof/'>LinkedIn</a>. If you would like to see his work firsthand, feel free to visit his <a className='underline' target='_blank' href='https://github.com/senyc'>GitHub</a>.
+      If you are interested in learning more about his work history and experience, please contact him <a className='underline' target='_blank' href='/contact/resumeRequest'>here</a> to get a hold of his resume. If you would like to send him a message please visit this <a className='underline' target='_blank' href='/contact'>contact form</a>, and he will get back to you as soon as possible. If you would like you can also find him on <a className='underline' target='_blank' href='https://www.linkedin.com/in/kyler-bomhof/'>LinkedIn</a>. If you would like to see his work firsthand, feel free to visit his <a className='underline' target='_blank' href='https://github.com/senyc'>GitHub</a>.
     </div>
   );
 }
--- src/app/qualifications/page.tsx
+++ src/app/qualifications/page.tsx
@@ -10,7 +10,7 @@ export default function Experience() {
       </h1>
       <InternshipTwo />
       <InternshipOne />
-      <h1 className='border-b-text mb-3 mt-16 w-11/12 max-w-3xl border-b-2 pb-2 text-left text-3xl font-bold sm:text-4xl'>
+      <h1 className='border-b-text mb-1 mt-16 w-11/12 max-w-3xl border-b-2 pb-2 text-left text-3xl font-bold sm:text-4xl'>
         Education
       </h1>
       <Education />
