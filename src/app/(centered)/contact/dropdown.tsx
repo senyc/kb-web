c141f911cee85c79d83915d5649d7c5749c8f35b
senyc <95313103+senyc@users.noreply.github.com>
2024-03-09 16:34:59 -0500

    fix: correct issue where user goes to contact url with option that does not exist

--- src/app/(centered)/contact/dropdown.tsx
+++ src/app/(centered)/contact/dropdown.tsx
@@ -15,7 +15,8 @@ export default function FormDropdown({ id, defaultValue = "", options, selectedV
   const toggleOpen = () => {
     document.getElementById(id)?.removeAttribute('open');
   };
-
+  const currentValueIndex = options.findIndex((val) => val.value == selectedValue) 
+  const defaultValueIndex = options.findIndex((val) => val.value == defaultValue) 
   return (
     <>
       <OutsideClickHandler 
@@ -28,7 +29,7 @@ export default function FormDropdown({ id, defaultValue = "", options, selectedV
           role="button"
           className=" input input-bordered w-96 list-none pt-3 font-normal"
         >
-          {selectedValue == "" ? options[options.findIndex((val) => val.value == defaultValue)].label : options[options.findIndex((val) => val.value == selectedValue)].label}
+          {selectedValue == "" ? options[defaultValueIndex].label : options[currentValueIndex != -1 ? currentValueIndex : defaultValueIndex].label}
         </summary>
         <ul className="menu dropdown-content dark:bg-dark-header bg-light-header mt-2 rounded-md shadow">
           {options.map(currentOption => {
